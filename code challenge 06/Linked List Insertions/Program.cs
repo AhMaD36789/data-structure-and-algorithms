b0805e029338a4865995e790e0f9bedbc256f1c5
Ahmad Harhsheh <45499233+AhMaD36789@users.noreply.github.com>
2023-07-01 02:38:39 +0300

    new plz work

---
+++ code challenge 06/Linked List Insertions/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Linked_List_Insertions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ code challenge 06/linked list insertions test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace linked_list_insertions_test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
