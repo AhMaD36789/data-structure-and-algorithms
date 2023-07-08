8ceae38de4e7e99cb8271c3d6d0854a95577922c
Ahmad Harhsheh <45499233+AhMaD36789@users.noreply.github.com>
2023-07-08 18:19:33 +0300

    completed code tests and readme

---
+++ code challenge 08/README.md
@@ -0,0 +1,27 @@
+# zip two linked lists
+
+function that recieves two linked lists as input and return a linked list whose value alternate between list 1 and list 2
+
+## whiteboard process
+
+![](./1.png)
+
+## process and efficiency
+
+create new list
+create two node element and equate them to respective list heads
+while either node1 OR node2 != null
+add to new list
+return new list
+
+TIME O(n)
+SPACE O(1)
+
+## solution
+
+![](2.png)
+![](3.png)
+![](4.png)
+![](5.png)
+![](6.png)
+![](7.png)
--- code challenge 08/zipinkedListTEST/UnitTest1.cs
+++ code challenge 08/zipinkedListTEST/UnitTest1.cs
@@ -1,11 +1,72 @@
+using zip_two_linked_lists;
+
 namespace zipinkedListTEST
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void TestLinkedListZippingForEquallySizedLists()
         {
+            SinglyLinkedList list1 = new SinglyLinkedList();
 
+            list1.Append(1);
+            list1.Append(1);
+            list1.Append(1);
+
+            SinglyLinkedList list2 = new SinglyLinkedList();
+
+            list2.Append(2);
+            list2.Append(2);
+            list2.Append(2);
+
+            SinglyLinkedList zipped = Program.ZipLists(list1, list2);
+            string result = zipped.StringifyLinkedList();
+
+            Assert.Equal("{ 1 } -> { 2 } -> { 1 } -> { 2 } -> { 1 } -> { 2 } -> NULL", result);
+        }
+
+        [Fact]
+        public void TestLinkedListZippingList1BiggerThanList2()
+        {
+            SinglyLinkedList list1 = new SinglyLinkedList();
+
+            list1.Append(1);
+            list1.Append(1);
+            list1.Append(1);
+            list1.Append(1);
+
+            SinglyLinkedList list2 = new SinglyLinkedList();
+
+            list2.Append(2);
+            list2.Append(2);
+            list2.Append(2);
+
+            SinglyLinkedList zipped = Program.ZipLists(list1, list2);
+            string result = zipped.StringifyLinkedList();
+
+            Assert.Equal("{ 1 } -> { 2 } -> { 1 } -> { 2 } -> { 1 } -> { 2 } -> { 1 } -> NULL", result);
+        }
+
+        [Fact]
+        public void TestLinkedListZippingList2BiggerThanList1()
+        {
+            SinglyLinkedList list1 = new SinglyLinkedList();
+
+            list1.Append(1);
+            list1.Append(1);
+            list1.Append(1);
+
+            SinglyLinkedList list2 = new SinglyLinkedList();
+
+            list2.Append(2);
+            list2.Append(2);
+            list2.Append(2);
+            list2.Append(2);
+
+            SinglyLinkedList zipped = Program.ZipLists(list1, list2);
+            string result = zipped.StringifyLinkedList();
+
+            Assert.Equal("{ 1 } -> { 2 } -> { 1 } -> { 2 } -> { 1 } -> { 2 } -> { 2 } -> NULL", result);
         }
     }
 }
