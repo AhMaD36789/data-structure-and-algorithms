fb60747768a1fd467a84ab59261a4d1f6ecef455
Ahmad Harhsheh <45499233+AhMaD36789@users.noreply.github.com>
2023-07-08 17:20:03 +0300

    setting up repo

---
+++ code challenge 08/zip two linked lists/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace zip_two_linked_lists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
