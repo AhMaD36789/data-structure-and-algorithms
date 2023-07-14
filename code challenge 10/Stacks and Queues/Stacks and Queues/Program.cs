90fa9fcde46e192001a12323ee5c7e3a71c7c672
Ahmad Harhsheh <45499233+AhMaD36789@users.noreply.github.com>
2023-07-15 01:31:48 +0300

    readme

--- code challenge 10/Stacks and Queues/Stacks and Queues/Program.cs
+++ code challenge 10/Stacks and Queues/Stacks and Queues/Program.cs
@@ -1,10 +1,18 @@
 ﻿namespace Stacks_and_Queues
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            
+            Queue queue = new Queue();
+            queue.EnQueue(1);
+            queue.EnQueue(2);
+            queue.EnQueue(3);
+            string result = "";
+            result += Convert.ToString(queue.DeQueue());
+            result += Convert.ToString(queue.DeQueue());
+            result += Convert.ToString(queue.DeQueue());
+            Console.WriteLine(result);
         }
     }
 }
--- code challenge 10/Stacks and Queues/Stacks and QueuesTest/UnitTest1.cs
+++ code challenge 10/Stacks and Queues/Stacks and QueuesTest/UnitTest1.cs
@@ -1,11 +1,164 @@
 namespace Stacks_and_QueuesTest
 {
-    public class UnitTest1
+    public class StackQueueTesting
     {
         [Fact]
-        public void Test1()
+        public void StackPush()
         {
+            Stack stack = new Stack();
+            stack.Push(1);
+            var result = stack.Peek();
+            Assert.Equal(1, result);
 
+        }
+        [Fact]
+        public void StackMultiPush()
+        {
+            Stack stack = new Stack();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            string result = "";
+            result += Convert.ToString(stack.Pop());
+            result += Convert.ToString(stack.Pop());
+            result += Convert.ToString(stack.Pop());
+            Assert.True(result == "321");
+        }
+
+        [Fact]
+        public void StackPop()
+        {
+            Stack stack = new Stack();
+            stack.Push(1);
+            int result = 0;
+            result = stack.Pop();
+            Assert.Equal(1, result);
+        }
+
+        [Fact]
+        public void StackPopUntilEmpty()
+        {
+            Stack stack = new Stack();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Pop();
+            stack.Pop();
+            stack.Pop();
+            Assert.Null(stack.Top);
+        }
+
+        [Fact]
+        public void StackPeekNext()
+        {
+            Stack stack = new Stack();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Top = stack.Top.Next;
+            int result = stack.Peek();
+            Assert.Equal(2, result);
+        }
+
+        [Fact]
+        public void StackPeekTop()
+        {
+            Stack stack = new Stack();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            int result = stack.Peek();
+            Assert.Equal(3, result);
+        }
+
+        [Fact]
+        public void ExceptionThrownOnCallingPop()
+        {
+            Stack stack = new Stack();
+            Assert.Throws<Exception>(() => stack.Pop());
+        }
+
+        [Fact]
+        public void ExceptionThrownOnCallingPeek()
+        {
+            Stack stack = new Stack();
+            Assert.Throws<Exception>(() => stack.Pop());
+        }
+
+        [Fact]
+        public void EnQueueTesting()
+        {
+            Queue queue = new Queue();
+            queue.EnQueue(5);
+            Assert.Equal(queue.Front.Data, 5);
+        }
+
+        [Fact]
+        public void MultipleEnQueueTesting()
+        {
+            Queue queue = new Queue();
+            queue.EnQueue(1);
+            queue.EnQueue(2);
+            queue.EnQueue(3);
+            string result = "";
+            result += Convert.ToString(queue.DeQueue());
+            result += Convert.ToString(queue.DeQueue());
+            result += Convert.ToString(queue.DeQueue());
+            Assert.True(result == "123");
+        }
+
+        [Fact]
+        public void DeQueueTesting()
+        {
+            Queue queue = new Queue();
+            queue.EnQueue(1);
+            queue.EnQueue(2);
+            int result = queue.DeQueue();
+            Assert.Equal(result, 1);
+        }
+
+        [Fact]
+        public void QueuePeekTesting()
+        {
+            Queue queue = new Queue();
+            queue.EnQueue(1);
+            queue.EnQueue(2);
+            int result = queue.Peek();
+            Assert.Equal(result, 1);
+        }
+
+        [Fact]
+        public void MultiDeQueueTesting()
+        {
+            Queue queue = new Queue();
+            queue.EnQueue(1);
+            queue.EnQueue(2);
+            queue.EnQueue(3);
+            queue.DeQueue();
+            queue.DeQueue();
+            queue.DeQueue();
+            Assert.Null(queue.Front);
+        }
+
+        [Fact]
+        public void EmptyQueueInstance()
+        {
+            Queue queue = new Queue();
+            Assert.Null(queue.Front);
+        }
+
+        [Fact]
+        public void ExceptionThrownOnCallingDeQueue()
+        {
+            Queue queue = new Queue();
+            Assert.Throws<Exception>(() => queue.DeQueue());
+        }
+
+        [Fact]
+        public void ExceptionThrownOnCallingQueuePeek()
+        {
+            Queue queue = new Queue();
+            Assert.Throws<Exception>(() => queue.Peek());
         }
     }
 }
