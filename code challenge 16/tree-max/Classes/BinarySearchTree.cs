4b1c2e850542ec05bb325075a27f1d37fd71dd9f
Ahmad Harhsheh <45499233+AhMaD36789@users.noreply.github.com>
2023-08-01 00:20:07 +0300

    finished in 10 minutes

---
+++ code challenge 16/tree-max/Classes/BinarySearchTree.cs
@@ -0,0 +1,98 @@
+﻿namespace Trees.Classes
+{
+    public class BinarySearchTree : BinaryTree
+    {
+        public Node? Root { get; set; }
+
+        public BinarySearchTree()
+        {
+            Root = null;
+        }
+        public BinarySearchTree(int value)
+        {
+            Root = new Node(value);
+        }
+
+        public void Add(int value)
+        {
+            if (Root == null)
+            {
+                Root = new Node(value);
+                return;
+            }
+
+            Node node = Root;
+            while (true)
+            {
+                if (value < node.Value)
+                {
+                    if (node.Left == null)
+                    {
+                        node.Left = new Node(value);
+                        break;
+                    }
+                    else
+                    {
+                        node = node.Left;
+                    }
+                }
+                else
+                {
+                    if (node.Right == null)
+                    {
+                        node.Right = new Node(value);
+                        break;
+                    }
+                    else
+                    {
+                        node = node.Right;
+                    }
+                }
+            }
+        }
+
+        public bool Contains(int value)
+        {
+            Node node = Root;
+            while (node != null)
+            {
+                if (value == node.Value)
+                {
+                    return true;
+                }
+                else if (value < node.Value)
+                {
+                    node = node.Left;
+                }
+                else
+                {
+                    node = node.Right;
+                }
+            }
+            return false;
+        }
+
+        public int SortedMax()
+        {
+            if (Root == null)
+                throw new Exception("EMPTY TREE! add some elements");
+            Node node = Root;
+            int max = Root.Value;
+            while (node.Right != null)
+            {
+                node = node.Right;
+                max = node.Value;
+            }
+            return max;
+        }
+
+        public int UnSortedMax()
+        {
+            if (Root == null)
+                throw new Exception("EMPTY TREE! add some elements");
+            List<int> list = new List<int>();
+            int[] arr = InOrder(Root, list);
+            return arr[arr.Length - 1];
+        }
+    }
+}
---
+++ code challenge 16/tree-max/Classes/BinaryTree.cs
@@ -0,0 +1,35 @@
+﻿namespace Trees.Classes
+{
+    public class BinaryTree
+    {
+        public int[] PreOrder(Node node, List<int> list)
+        {
+            if (node == null)
+                return new int[0];
+            list.Add(node.Value);
+            PreOrder(node.Left, list);
+            PreOrder(node.Right, list);
+            return list.ToArray();
+        }
+
+        public int[] InOrder(Node node, List<int> list)
+        {
+            if (node == null)
+                return new int[0];
+            InOrder(node.Left, list);
+            list.Add(node.Value);
+            InOrder(node.Right, list);
+            return list.ToArray();
+        }
+
+        public int[] PostOrder(Node node, List<int> list)
+        {
+            if (node == null)
+                return new int[0];
+            PostOrder(node.Left, list);
+            PostOrder(node.Right, list);
+            list.Add(node.Value);
+            return list.ToArray();
+        }
+    }
+}
