7eae2c3361cb5d46686513a49a8eacdc693bf222
Ahmad Harhsheh <45499233+AhMaD36789@users.noreply.github.com>
2023-08-09 09:33:18 +0300

    finished

---
+++ code challenge 18/README.md
@@ -0,0 +1,19 @@
+# FizzBuzz Kary Tree
+
+this is a method to check if the value in a node is divisable by 3 or 5 or both
+
+## whiteboard
+
+![](1.png)
+
+## process and efficiency
+
+Time O(n)
+
+Space O(n)
+
+## Solution
+
+![](2.png)
+![](3.png)
+![](4.png)
---
+++ code challenge 18/tree-fizz-buzz/Classes/Node.cs
@@ -0,0 +1,22 @@
+﻿namespace tree_breadth_first.classes
+{
+    public class Node
+    {
+        public int Value { get; set; }
+        public Node? Left { get; set; }
+        public Node? Right { get; set; }
+
+        public Node()
+        {
+            Left = null;
+            Right = null;
+        }
+
+        public Node(int value)
+        {
+            Value = value;
+            Left = null;
+            Right = null;
+        }
+    }
+}
