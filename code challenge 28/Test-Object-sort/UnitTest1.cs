0333f49d7989424c59fc69e59a11c99b70702502
Ahmad Harhsheh <45499233+AhMaD36789@users.noreply.github.com>
2023-09-09 22:16:55 +0300

    2nd submission finished 2nd unit test

--- code challenge 28/Test-Object-sort/UnitTest1.cs
+++ code challenge 28/Test-Object-sort/UnitTest1.cs
@@ -32,5 +32,33 @@ namespace Test_Object_sort
 
             Assert.Equal(expected, SortedByYear);
         }
+
+        [Fact]
+        public void TitleSortAlgorithm()
+        {
+            //override for assert.equal
+
+            Movie[] movies = new Movie[5]
+        {
+            new Movie { title = "A Movie", year = 2012, genres = new List<string> { "Action", "Adventure" } },
+            new Movie { title = "An Amazing Movie", year = 2011, genres = new List<string> { "Comedy", "Romance" } },
+            new Movie { title = "The bestest Movie", year = 2003, genres = new List<string> { "Drama", "Thriller" } },
+            new Movie { title = "Zelda", year = 2020, genres = new List<string> { "Sci-Fi", "Mystery" } },
+            new Movie { title = "Starfield", year = 2001, genres = new List<string> { "Horror", "Fantasy" } }
+        };
+
+            Movie[] SortedByYear = movies[0].SortArrayMoviesByTitle(movies);
+
+            Movie[] expected = new Movie[5]
+            {
+            new Movie { title = "An Amazing Movie", year = 2011, genres = new List<string> { "Comedy", "Romance" } },
+            new Movie { title = "The bestest Movie", year = 2003, genres = new List<string> { "Drama", "Thriller" } },
+            new Movie { title = "A Movie", year = 2012, genres = new List<string> { "Action", "Adventure" } },
+            new Movie { title = "Starfield", year = 2001, genres = new List<string> { "Horror", "Fantasy" } },
+            new Movie { title = "Zelda", year = 2020, genres = new List<string> { "Sci-Fi", "Mystery" } },
+            };
+
+            Assert.Equal(expected, SortedByYear);
+        }
     }
 }
